202587d6bd2c078b182681397e90804e053675fc
Viraj <77448246+virajsabhaya23@users.noreply.github.com>
2022-03-04 15:35:24 -0600

    file reading

--- Automation/fileIO.py
+++ Automation/fileIO.py
@@ -1,3 +1,8 @@
 f = open('inputFile.txt', 'r')
-print(f.read())
+counter = 0
+for line in f:
+    line_split = line.split()
+    if line_split[2] == "P":
+        print(str(counter) + line)
+        counter = counter+1
 f.close()

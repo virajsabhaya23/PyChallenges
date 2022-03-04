83c2f1b4ff8e47837514c10f4e375285ad445a05
Viraj <77448246+virajsabhaya23@users.noreply.github.com>
2022-03-04 16:50:41 -0600

    Scrapping from quotes website

--- Automation/WebScrapping/Scrape.py
+++ Automation/WebScrapping/Scrape.py
@@ -5,4 +5,15 @@ url = 'https://quotes.toscrape.com/'
 response = requests.get(url)
 
 soup = BeautifulSoup(response.text,'lxml')
-print(soup)
+
+quotes = soup.find_all('span', class_='text')
+authors = soup.find_all('small', class_='author')
+tags = soup.find_all('div', class_='tags')
+
+
+for i in range(0, len(quotes)):
+    print(quotes[i].text)
+    print(authors[i].text)
+    quoteTags = tags[i].find_all('a',class_='tag')
+    for quoteTag in quoteTags:
+        print(quoteTag.text)

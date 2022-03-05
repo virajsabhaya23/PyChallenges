db93cc3f9a7e6dea51c917a5835762333967eac4
Viraj <77448246+virajsabhaya23@users.noreply.github.com>
2022-03-05 01:09:25 -0600

    automate APIs with JSON

---
+++ Automation/AutomateAPIs/apiCall.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+baseUrl = 'https://api.upcitemdb.com/prod/trial/lookup'
+parameters = {'upc': '012993441012'}
+
+response = requests.get(baseUrl, params=parameters)
+print(response.url)
+
+content = response.content
+info = json.loads(content)  #load the JSON content into python friendly dictionaries
+print(type(info))
+print(info)
+
+item = info['items']
+itemInfo = item[0]
+title = itemInfo['title']
+brand = itemInfo['brand']
+
+print(title)
+print(brand)
+

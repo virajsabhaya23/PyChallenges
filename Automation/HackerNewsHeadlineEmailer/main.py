ffddc0d21da9a1fb2969ffc407988dcb44183d1a
Viraj <77448246+virajsabhaya23@users.noreply.github.com>
2022-04-06 04:39:23 -0500

    HNS emailer final update

--- Automation/HackerNewsHeadlineEmailer/main.py
+++ Automation/HackerNewsHeadlineEmailer/main.py
@@ -15,12 +15,50 @@ content = ''
 
 #EXTRACTING NEWS STORIES
 def extract_news(url):
-    print("\nEXTRACTING HACKER NEWS STORIES ...")
+    print('Extracting Hacker News Stories...')
     cnt = ''
-    cnt += ('<b>HN TOP STORIES:</b>\n' + '<br>' + '-' * 50 + '<br>')
+    cnt +=('<b>HN Top Stories:</b>\n'+'<br>'+'-'*50+'<br>')
     response = requests.get(url)
     content = response.content
-    soup = BeautifulSoup(response,'html.parser')
-
+    soup = BeautifulSoup(content,'html.parser')
     for i,tag in enumerate(soup.find_all('td',attrs={'class':'title','valign':''})):
-        cnt += ((str(i+1) + ' :: '))
+        cnt += ((str(i+1)+' :: '+ '<a href="' + tag.a.get('href') + '">' + tag.text + '</a>' + "\n" + '<br>') if tag.text!='More' else '')
+        #print(tag.prettify) #find_all('span',attrs={'class':'sitestr'}))
+    return(cnt)
+
+cnt = extract_news('https://news.ycombinator.com/')
+content += cnt
+content += ('<br>------<br>')
+content += ('<br><br>END OF MESSAGE !')
+
+print('Composing Email...')
+
+SERVER = 'smtp.gmail.com' # "your smtp server"
+PORT = 587 # your port number
+FROM =  '' # "your from email id"
+TO = '' # "your to email ids"  # can be a list
+PASS = '*****' # "your email id's password"
+
+msg = MIMEMultipart()
+
+msg['Subject'] = 'Top News Stories HN [Automated Email]' + ' ' + str(now.day) + '-' + str(now.month) + '-' + str(
+    now.year)
+msg['From'] = FROM
+msg['To'] = TO
+
+msg.attach(MIMEText(content, 'html'))
+
+print('Initiating Server...')
+
+server = smtplib.SMTP(SERVER, PORT)
+#server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+server.set_debuglevel(1)
+server.ehlo()
+server.starttls()
+#server.ehlo
+server.login(FROM, PASS)
+server.sendmail(FROM, TO, msg.as_string())
+
+print('Email Sent...')
+
+server.quit()
---
+++ Automation/HackerNewsHeadlineEmailer/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+
+# SERVER = 'smtp.gmail.com' # "your smtp server"
+# PORT = 587 # your port number
+# FROM =  '' # "your from email id"
+# TO = '' # "your to email ids"  # can be a list
+# PASS = '*****' # "your email id's password"

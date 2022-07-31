30d7ce1a9a82430d90fedb07f27efe6260dbffdc
Viraj <77448246+virajsabhaya23@users.noreply.github.com>
2022-07-31 17:16:24 -0500

    generatedJSON

--- Automation/ExcelToJSON/csvToJson.py
+++ Automation/ExcelToJSON/csvToJson.py
@@ -1,17 +1,26 @@
+import json,csv
+
+with open ("sample.csv","r") as f:
+    reader = csv.reader(f)
+    next(reader)
+    data = []
+    for row in reader:
+        data.append({"segment":row[0],
+                     "country":row[1],
+                     "product":row[2]})
+
+with open ("sample.json","w") as f:
+    json.dump(data,f,indent=4)
+
 # import pandas as pd
-
 # filePath = r"/Users/virajsabhaya/Library/CloudStorage/OneDrive-UniversityofTexasatArlington/Documents/MyPersonalProject/PyChallenges/Automation/ExcelToJSON/FinancialSample.xlsx"
-
 # df = pd.read_excel(filePath)
-
 # segment = df.Segment
 # country = df.Country
 # product = df.Product
 # discount = df.DiscountBand
 # unitsSold = df.UnitsSold
-
 # jsonContainer = {}
-
 # x = 0
 # while x < len(segment):
 #     jsonContainer[segment[x]] = [
@@ -22,11 +31,10 @@
 #         {"unitsSold: ": unitsSold[x]}
 #     ]
 #     x = x+1
-
 # dF = pd.DataFrame(jsonContainer)
 # print(dF)
 
-from excel2json import convert_from_file
 
-xlFile = '/Users/virajsabhaya/Library/CloudStorage/OneDrive-UniversityofTexasatArlington/Documents/MyPersonalProject/PyChallenges/Automation/ExcelToJSON/FinancialSample.xlsx'
-convert_from_file(xlFile)
+# from excel2json import convert_from_file
+# xlFile = '/Users/virajsabhaya/Library/CloudStorage/OneDrive-UniversityofTexasatArlington/Documents/MyPersonalProject/PyChallenges/Automation/ExcelToJSON/FinancialSample.xlsx'
+# convert_from_file(xlFile)
